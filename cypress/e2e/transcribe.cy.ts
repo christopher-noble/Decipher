1be59206a080f02a9aad53e7dccbd9e1fce9e244
Admin <admin@Chris-MacBook-Pro.local>
2024-02-23 12:41:17 -0800

    Increased timeout to 40 seconds for cypress test

--- cypress/e2e/transcribe.cy.ts
+++ cypress/e2e/transcribe.cy.ts
@@ -4,6 +4,6 @@ describe('template spec', () => {
     cy.get('.tag-area').type('jamie pull that up');
     cy.get('[name=url-input]').type('https://www.youtube.com/watch?v=SRgct4c5T5U');
     cy.get('button.submit').click();
-    cy.get('#controlTextarea1', { timeout: 30000 }).should('be.disabled'); // wait 30 seconds for the job to complete
+    cy.get('#controlTextarea1', { timeout: 40000 }).should('be.disabled'); // wait 30 seconds for the job to complete
   })
 })
