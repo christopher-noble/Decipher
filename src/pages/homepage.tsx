a45470e88dca82b183095b64c879539322892a53
Admin <admin@Chris-MacBook-Pro.local>
2024-02-27 16:42:02 -0800

    Fixed merge issue

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,5 +1,5 @@
 import './styles/homePageStyles.css';
-import HOMEPAGE_INFO from '../data/homepage';
+import HOMEPAGE_INFO from '../data/homePage';
 import HeadingButton from '../components/common/buttons/headingButton';
 
 const HomePage = () => {
